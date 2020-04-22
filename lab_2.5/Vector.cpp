1dda9acd6de0ab52d0230b4c7d32d8fb0395b3ca
freesummerwind <liz-e-tta@yandex.ru>
2020-04-22 16:29:48 +0300

    Add constructors and destructor

--- lab_2.5/Vector.cpp
+++ lab_2.5/Vector.cpp
@@ -1,1 +1,10 @@
 #include "Vector.h"
+
+template <class T>
+Vector<T>::Vector(const Vector<T>& v)
+{
+	size = v.size;
+	capasity = v.capasity;
+	elements = new T[capasity];
+	for (int i = 0; i < size; ++i) elements[i] = v.elements[i];
+}
--- lab_2.5/Vector.h
+++ lab_2.5/Vector.h
@@ -1,5 +1,14 @@
 #pragma once
+template <class T>
 class Vector
 {
+	int size, capasity;
+	T* elements;
+	void upgradeSize();
+public:
+	Vector() : size(0), capasity(0), elements(nullptr) {}
+	Vector(const Vector&);
+	~Vector() { delete[] elements; }
+
 };
 
