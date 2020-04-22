60cbc22f98a1c1d5ecbfac7b3b80a16f8c71f89d
freesummerwind <liz-e-tta@yandex.ru>
2020-04-22 18:51:18 +0300

    Add size, reserve, capacity, clear, insert methods

--- lab_2.5/Vector.cpp
+++ lab_2.5/Vector.cpp
@@ -3,13 +3,13 @@
 template <class T>
 Vector<T>::Vector(const Vector<T>& v)
 {
-	size = v.size;
-	capasity = v.capasity;
+	sizeOfVector = v.sizeOfVector;
+	capacityOfVector = v.capacityOfVector;
 	if (v.elements = nullptr) elements = nullptr;
 	else
 	{
-		elements = new T[capasity];
-		for (int i = 0; i < size; ++i) elements[i] = v.elements[i];
+		elements = new T[capacityOfVector];
+		for (size_t i = 0; i < sizeOfVector; ++i) elements[i] = v.elements[i];
 	}
 }
 
@@ -19,13 +19,13 @@ Vector<T>& Vector<T>::operator=(const Vector<T>& v)
 	if (*this != v)
 	{
 		if(elements != nullptr) delete[] elements;
-		size = v.size;
-		capasity = v.capasity;
+		sizeOfVector = v.sizeOfVector;
+		capacityOfVector = v.capacityOfVector;
 		if (v.elements = nullptr) elements = nullptr;
 		else
 		{
-			elements = new T[capasity];
-			for (int i = 0; i < size; ++i) elements[i] = v.elements[i];
+			elements = new T[capacityOfVector];
+			for (size_t i = 0; i < sizeOfVector; ++i) elements[i] = v.elements[i];
 		}
 	}
 	return *this;
@@ -34,84 +34,125 @@ Vector<T>& Vector<T>::operator=(const Vector<T>& v)
 template <class T>
 T& Vector<T>::operator[](const size_t pos)
 {
-	if (pos < size) return elements[pos];
+	if (pos < sizeOfVector) return elements[pos];
 }
 
 template <class T>
 const T& Vector<T>::operator[](const size_t pos) const
 {
-	if (pos < size) return elements[pos];
-}
-
-template <class T>
-void Vector<T>::upgradeSize()
-{
-	if (size = 1) capasity = 1;
-	if (size = 2) capasity = 2;
-	if (size > 2) capasity = (size - 1) * 1.5;
-	T* elem = new T[capasity];
-	if (elements != nullptr)
-	{
-		for (int i = 0; i < size - 1; ++i) elem[i] = elements[i];
-		delete[] elements;
-	}
-	elements = elem;
+	if (pos < sizeOfVector) return elements[pos];
 }
 
 template <class T>
 T& Vector<T>::at(const size_t pos)
 {
-	if (!(pos < size)) throw std::out_of_range;
+	if (!(pos < sizeOfVector)) throw std::out_of_range;
 	return elements[pos];
 }
 
 template <class T>
 const T& Vector<T>::at(const size_t pos) const
 {
-	if (!(pos < size)) throw std::out_of_range;
+	if (!(pos < sizeOfVector)) throw std::out_of_range;
 	return elements[pos];
 }
 
 template <class T>
 T& Vector<T>::front()
 {
-	if (size != 0) return elements[0];
+	if (sizeOfVector != 0) return elements[0];
 }
 
 template <class T>
 const T& Vector<T>::front() const
 {
-	if (size != 0) return elements[0];
+	if (sizeOfVector != 0) return elements[0];
 }
 
 template <class T>
 T& Vector<T>::back()
 {
-	if (size != 0) return elements[size - 1];
+	if (sizeOfVector != 0) return elements[size - 1];
 }
 
 template <class T>
 const T& Vector<T>::back() const
 {
-	if (size != 0) return elements[size - 1];
+	if (sizeOfVector != 0) return elements[size - 1];
 }
 
 template <class T>
 T* Vector<T>::data() noexcept
 {
-	if (size != 0) return &(elements[0]);
+	if (sizeOfVector != 0) return &(elements[0]);
 	else return nullptr;
 }
 
 template <class T>
 const T* Vector<T>::data() const noexcept
 {
-	if (size != 0) return &(elements[0]);
+	if (sizeOfVector != 0) return &(elements[0]);
 	else return nullptr;
 }
 
 template <class T>
-bool Vector<T>::empty() const
+bool Vector<T>::empty() const noexcept
 {
-	return (size == 0);
+	return (sizeOfVector == 0);
+}
+
+template <class T>
+size_t Vector<T>::size() const noexcept
+{
+	return sizeOfVector;
+}
+
+template <class T>
+void Vector<T>::reserve(size_t new_cap)
+{
+	if (new_cap <= capacityOfVector) return;
+	if (new_cap > std::numeric_limits<size_t>::max()) throw std::length_error;
+	capacityOfVector = new_cap;
+	T* elem = new T[capacityOfVector];
+	if (elements != nullptr)
+	{
+		for (size_t i = 0; i < sizeOfVector; ++i) elem[i] = elements[i];
+		delete[] elements;
+	}
+	elements = elem;
+}
+
+template <class T>
+size_t Vector<T>::capacity() const noexcept
+{
+	return capacityOfVector;
+}
+
+template <class T>
+void Vector<T>::clear() noexcept
+{
+	sizeOfVector = 0;
+	capacityOfVector = 0;
+	delete[] elements;
+	elements = nullptr;
+}
+
+template <class T>
+void Vector<T>::insert(const size_t pos, const T& value)
+{
+	if (sizeOfVector + 1 > capacityOfVector)
+	{
+		if (capacityOfVector = 0) capacityOfVector = 1;
+		if (capacityOfVector = 1) capacityOfVector = 2;
+		if (capacityOfVector > 2) capacityOfVector = sizeOfVector * 1.5;
+		reserve(capacityOfVector);
+	}
+	++sizeOfVector;
+	for (size_t i = pos; i < sizeOfVector; ++i)
+	{
+		size_t temp = elements[i];
+		elements[i] = value;
+		value = temp;
+	}
+	value = elements[pos];
 }
--- lab_2.5/Vector.h
+++ lab_2.5/Vector.h
@@ -2,11 +2,10 @@
 template <class T>
 class Vector
 {
-	size_t size, capasity;
+	size_t sizeOfVector, capacityOfVector;
 	T* elements;
-	void upgradeSize();
 public:
-	Vector() : size(0), capasity(0), elements(nullptr) {}
+	Vector() : sizeOfVector(0), capacityOfVector(0), elements(nullptr) {}
 	Vector(const Vector&);
 	~Vector() { delete[] elements; }
 	Vector& operator=(const Vector&);
@@ -20,5 +19,10 @@ public:
 	const T& back() const;
 	T* data() noexcept;
 	const T* data() const noexcept;
-	bool empty() const;
+	bool empty() const noexcept;
+	size_t size() const noexcept;
+	void reserve(size_t);
+	size_t capacity() const noexcept;
+	void clear() noexcept;
+	void insert(const size_t, const T&);
 };
